b468247f71e247275b07a1f547cd6443cca97ef7
Jeremy Broutin <jeremybroutin@jeremybroutin-macbookpro.roam.corp.google.com>
2016-07-01 15:30:42 -0700

    Solve Bronze, Silver and Gold Challenges in separate project

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -15,6 +15,9 @@ class ItemsViewController: UITableViewController {
 	// To implement the DIP, we use dependency injection, where the lower-level object (the store)
 	// is passed to the higher-level oject (ItemsVC) through a property.
 	
+	let sectionTitles = ["Below $50", "Equal and Above $50"]
+	var bidimensionalArray = [[Item]]()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
@@ -23,26 +26,121 @@ class ItemsViewController: UITableViewController {
 		let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
 		tableView.contentInset = insets
 		tableView.scrollIndicatorInsets = insets
+		
+		splitItemsArray(itemStore.allItems)
+		
 	}
+	
+	override func viewWillAppear(animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		let backgroundImage = UIImage(named: "tableviewBackground")
+		let imageView = UIImageView(image: backgroundImage)
+		imageView.contentMode = .ScaleAspectFill
+		//imageView.alpha = 0.5
+		tableView.backgroundView = imageView
+		
+		tableView.tableFooterView = UIView(frame: CGRectZero)
+	}
+	
+	// Bronze challenge: display two sections
+	// One for items >$50 and one for the rest
+	
+	// Silver Challenge: add a static cell at the bottom of dynamic cell
+	// to display "No more" label
+	
+	// Gold Challenge
+	//
+	
+	// 1 - Split and re join array in two dimensional array
+	
+	func splitItemsArray(arr: [Item]) -> [[Item]]{
+		var below50Array = [Item]()
+		var above50Array = [Item]()
+		
+		for el in arr {
+			if el.valueInDollars < 50 {
+					below50Array.append(el)
+			} else if el.valueInDollars >= 50 {
+				above50Array.append(el)
+			}
+		}
+		
+		bidimensionalArray.append(below50Array)
+		bidimensionalArray.append(above50Array)
+		
+		return bidimensionalArray
+	}
+	
+	override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+		return sectionTitles.count + 1 // adding last section & row "No more"
+	}
+	
+	override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+		if section < sectionTitles.count {
+			return sectionTitles[section]
+		} else {
+			return nil
+		}
+	}
+	
+	// 2 - Use the newly populated bidimensional array to count rows in section
 	
 	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return itemStore.allItems.count
+		if section < sectionTitles.count {
+			return bidimensionalArray[section].count
+		} else {
+			return 1
+		}
+		
 	}
 	
+	// 3 - Use the newly populated bidimensional array to define the item based on both section and row indexes
+	
 	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-		// Create an instance of UITableViewCell with default appearance
-		let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
+		
 		// dequeue... will check the pool to see wether a cell withthe correct reuse identifier already exists.
 		// If so it will dequeue this cell. If not, a new cell will be created an returned.
 		
-		// Set the text on the cell with the description of the item that is at the nth index of items,
-		// where n = row this cell will appear in on the tableview
-		let item = itemStore.allItems[indexPath.row]
-		
-		cell.textLabel?.text = item.name
-		cell.detailTextLabel?.text = "$\(item.valueInDollars)"
-		
-		return cell
+		if indexPath.section < sectionTitles.count {
+			let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
+			
+			// Set the text on the cell with the description of the item that is at the nth index of items,
+			// where n = row this cell will appear in on the tableview
+			let item = bidimensionalArray[indexPath.section][indexPath.row]
+			
+			cell.textLabel?.text = item.name
+			cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+			
+			return cell
+		}
+		else {
+			let cell = tableView.dequeueReusableCellWithIdentifier("UniqueCell", forIndexPath: indexPath)
+			cell.textLabel?.text = "No more items!"
+			cell.textLabel?.font = UIFont.boldSystemFontOfSize(17)
+			cell.textLabel?.textAlignment = .Center
+			return cell
+		}
+	}
+	
+	// Change dynamic row height
+	override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+		if indexPath.section < sectionTitles.count {
+			return 60
+		} else {
+			return 30 // default is 44
+		}
+	}
+	
+	// Make cells transparent
+	override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+		if indexPath.section < sectionTitles.count {
+			cell.backgroundColor = UIColor(white: 1, alpha: 0.5)
+			cell.textLabel?.backgroundColor = .clearColor()
+			cell.detailTextLabel?.backgroundColor = .clearColor()
+		} else {
+			cell.textLabel?.textColor = UIColor.darkGrayColor()
+		}
 	}
 	
 }
